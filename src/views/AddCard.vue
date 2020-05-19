dc403c1f8a221b7afc4092c24a4cfbeb9d37bad6
Ivan Jurkovic <icasamoubica@yahoo.co.uk>
2020-05-19 10:47:09 +0200

    removed old validation code

--- src/views/AddCard.vue
+++ src/views/AddCard.vue
@@ -74,98 +74,9 @@ export default {
                 this.$root.addCard(this.newCard)
                 this.$router.push('/')
             }
-
-            // resetting validation values
-
-            // this.cardValidations = {
-            //     "cardNumberFormat" : false,
-            //     "cardNumberEmpty" : false,
-            //     "nameFormat" : false,
-            //     "nameEmpty" : false,
-            //     "validThruFormat" : false,
-            //     "validThruEmpty" : false,
-            //     "ccvFormat" : false,
-            //     "ccvEmpty" : false,
-            //     "vendorEmpty" : false
-            // }
-
-            // // validating card number
-            // if (this.newCard.cardNumber.length === 0) {
-            //     this.cardValidations.cardNumberEmpty = true
-            // } else if (this.newCard.cardNumber.length != 19) {
-            //     this.cardValidations.cardNumberFormat = true
-            // } else if (!(Number.isInteger(parseInt(this.newCard.cardNumber.substring(0, 4)), 10) 
-            //         && Number.isInteger(parseInt(this.newCard.cardNumber.substring(5, 9)), 10)
-            //         && Number.isInteger(parseInt(this.newCard.cardNumber.substring(10, 14)), 10)
-            //         && Number.isInteger(parseInt(this.newCard.cardNumber.substring(15, 19)), 10))) {
-            //     this.cardValidations.cardNumberFormat = true
-            // } else {
-            //     this.cardValidations.cardNumberFormat = false
-            // }
-            // // validating cardholder name
-            // if (this.newCard.carholderName.length===0) {
-            //     this.cardValidations.nameEmpty = true
-            // } else if (!this.newCard.carholderName.trim().includes(' ')) {
-            //     this.cardValidations.nameFormat = true
-            // } else {
-            //     this.cardValidations.nameFormat = false
-            // }
-            // // validating valid thru
-            // if (this.newCard.validUntil.length === 0) {
-            //     this.cardValidations.validThruEmpty = true
-            // } else if (this.newCard.validUntil.length != 5) {
-            //     this.cardValidations.validThruFormat = true
-            // } else if (!(   Number.isInteger(parseInt(this.newCard.validUntil.substring(0,1)))
-            //             && Number.isInteger(parseInt(this.newCard.validUntil.substring(3,4))))) {
-
-            //     this.cardValidations.validThruFormat = true
-            // } else if (!this.newCard.validUntil.trim().includes('/')) {
-            //     this.cardValidations.validThruFormat = true
-            // } else {
-            //     this.cardValidations.validThruFormat = false
-            // }
-            // // validating ccv
-            // if (this.newCard.ccv.length === 0) {
-            //     this.cardValidations.ccvEmpty = true
-            // } else if (this.newCard.ccv.length != 3) {
-            //     this.cardValidations.ccvEmpty = true
-            // } else if (!(Number.isInteger(parseInt(this.newCard.ccv)))) {
-            //     this.cardValidations.ccvEmpty = true
-            // } else {
-            //     this.cardValidations.ccvEmpty = false
-            // }
-            // // validating vendor
-            // if (this.newCard.vendor.name === 'none') {
-            //     this.cardValidations.vendorEmpty = true
-            // } else {
-            //     this.cardValidations.vendorEmpty = false
-            // }
-            // if (this.allValidationsPassed()) {
-            //     console.log('validations passed');
-            //     this.$root.addCard(this.newCard)
-            //     this.$router.push('/')
-            // }
         },
         allValidationsPassed() {
-
             return Validations.allValidationsPassed(this.cardValidations)
-
-            // console.log('running validations');
-            
-            // if (this.cardValidations.cardNumberFormat === false &&
-            //     this.cardValidations.cardNumberEmpty === false &&
-            //     this.cardValidations.nameFormat === false &&
-            //     this.cardValidations.nameEmpty === false &&
-            //     this.cardValidations.validThruFormat === false &&
-            //     this.cardValidations.validThruEmpty === false &&
-            //     this.cardValidations.ccvFormat === false &&
-            //     this.cardValidations.ccvEmpty === false &&
-            //     this.cardValidations.vendorEmpty === false) {
-            //     console.log('everything in order');
-            //     return true
-            // } else {
-            //     return false
-            // }
         }
     },
 
